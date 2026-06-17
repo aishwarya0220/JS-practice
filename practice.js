6fb4e83bd6df18f8720023b3e40e6d93b5e45bf4
Aishwarya Shrivastava <78354656+aishwarya0220@users.noreply.github.com>
2026-06-17 17:50:00 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,102 @@
+// let myCourses = ["HTML","CSS","JS","TS"]
+
+// function logValue(arr) {                    // parameter and argument
+//     for (let i=0;i < arr.length ;i++){
+//         console.log(arr[i])
+//     }
+// }
+
+// logValue(myCourses)
+
+
+
+
+
+
+// let details = {
+//     name: "Japesh",
+//     age: 21,
+//     gay: false                           // localStorage setting & getting values
+// }
+
+// localStorage.setItem("user", JSON.stringify(details)) //lS.sI("asdasd","uyuyuy")
+
+// let info = JSON.parse(localStorage.getItem("user"))
+
+// console.log(info)
+
+
+
+
+
+
+// let jaiEL = document.getElementById("jai")
+
+// let data = [
+//     {
+//         player: "Jai",
+//         score: 45
+//     },
+//     {
+//         player: "Lavish",               // Retrieve value from object within an array
+//         score: 65
+//     }
+// ]
+
+// jaiEL.addEventListener("click", function(){
+//     console.log(data[0])
+// })
+
+
+
+
+
+
+// let countries = ["India","China","USA"]
+
+// let fruits = ["Apple","Banana"]
+
+// function generateSentence(desc, arr) {
+//     let string = `The ${arr.length} ${desc} are ${arr}`     // template literals
+//     console.log(string)
+// }
+
+// generateSentence("largest countries", countries)
+
+
+
+
+
+
+// const imgs = [
+//     "amitabh.png",
+//     "nigga.jpg"
+// ]
+
+// let space = document.getElementById("greeting")
+
+// function imageDisplay() {                                   // Images from innerHTML
+//     for (let i=0; i < imgs.length; i++){
+//     space.innerHTML += `<img class="team-img" src="${imgs[i]}">`
+//     }
+// }
+
+// imageDisplay()
+
+
+
+
+
+
+
+// const price = "24.968769778"
+                                                    // Rounding off the decimal
+// const buy = document.getElementById("jai")
+
+// buy.textContent = `Buy ${Number(Math.round(price*100)/100)}`       // ${price.toFixed(2)}
+
+
+
+
+
+
